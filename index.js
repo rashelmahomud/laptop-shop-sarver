f21567b358658e12c181d7b49453fa8f44f21c01
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-09-24 13:51:48 +0600

    update code

--- index.js
+++ index.js
@@ -24,21 +24,21 @@ const client = new MongoClient(uri, {
 });
 
 //veryfy JWT starte here
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: "accessToken forbiden" });
-    }
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({ message: "accessToken forbiden" });
+//     }
 
-    req.decoded = decoded;
-    next();
-  });
-}
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
 //veryfy JWT ends here
 
 async function run() {
